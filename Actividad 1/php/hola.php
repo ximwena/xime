48a60cf1e3f3be36e4d657b5cfe732d4024ffd22
ximwena <114588332+ximwena@users.noreply.github.com>
2022-10-26 19:07:47 -0500

    Create hola.php

---
+++ Actividad 1/php/hola.php
@@ -0,0 +1,26 @@
+<!-- 
+    Descripcion:Hola Soy php
+    Autor:Alondra Ximena Gonzales Romero
+    Fecha:15/10/2022
+ -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilos.css">
+    <title>Hola</title>
+</head>
+<body>
+    <div class="titulo">
+    <?php
+      echo"<center>";
+    echo"<h1>";
+    echo"Hola Soy PHP";
+    echo"</h1>";
+    echo"</center>";
+    ?>
+    </div>
+</body>
+</html>
